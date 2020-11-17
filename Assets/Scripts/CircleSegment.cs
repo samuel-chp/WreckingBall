5da44bd1d455dbc4b936f436da7fb1c88a8d7234
TanNav <tanguynavez@yahoo.fr>
2020-11-17 16:44:02 +0100

    Puzzle Game Gameplay

--- Assets/Scripts/CircleSegment.cs
+++ Assets/Scripts/CircleSegment.cs
@@ -15,13 +15,19 @@ public class CircleSegment : MonoBehaviour
     private float _startAngle;
     private float _endAngle;
 
-    public void Initialize(float innerRadius, float outerRadius, float startAngle, float endAngle)
+    // To keep track of the location of the circle Segment
+    private int _slice;
+    private int _layer;
+
+    public void Initialize(float innerRadius, float outerRadius, float startAngle, float endAngle, int slice, int layer)
     {
         float angleOffset = Mathf.Asin(offset / innerRadius);
         _innerRadius = innerRadius + offset;
         _outerRadius = outerRadius - offset;
         _startAngle = startAngle+angleOffset;
         _endAngle = endAngle-angleOffset;
+        _slice = slice;
+        _layer = layer;
         
         _polygonCollider = GetComponent<PolygonCollider2D>();
         
@@ -47,12 +53,68 @@ public class CircleSegment : MonoBehaviour
         _polygonCollider.points = points;
     }
 
+
+    // Collider only with first segment encountered for now
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Projectile"))
-        {
+        {   
+            bool collision_handled = false; 
+
+            // Access to circleSegmentManager attributes
+            CircleSegmentManager circleSegmentManager = GameObject.Find("Planet Bottom").GetComponent<CircleSegmentManager>(); 
+
+            // If the first layer encountered is coloured (not of the color describing an empty block), then the player loses
+            if (circleSegmentManager.segmentsOrdered[_slice, circleSegmentManager.nLayer - 1].GetComponent<SpriteRenderer>().color != circleSegmentManager.segmentColors[0]) {
+                // ----- TODO: Implement that the player can lose ----- //
+                // PlayerLoses();
+                collision_handled = true;
+                Destroy(other.gameObject);
+
+            // Normal case
+            } else if (!collision_handled) {
+
+                // Loop on all segments (layer) of the current slice to find the uncolored segment that is the closest one
+                for (int i = circleSegmentManager.nLayer - 2; i >= 0 ; i--) {
+                    if (!collision_handled && circleSegmentManager.segmentsOrdered[_slice, i].GetComponent<SpriteRenderer>().color != circleSegmentManager.segmentColors[0]) {
+
+                        // Change colour of block by colour of ball
+                        circleSegmentManager.segmentsOrdered[_slice, i+1].GetComponent<SpriteRenderer>().color = other.GetComponent<SpriteRenderer>().color;
+
+                        // Update color Array
+                        circleSegmentManager.colorBlocks[_slice, i+1] = other.GetComponent<SpriteRenderer>().color;
+
+                        // ----- TODO: The ball must disappear at the good spot -----
+                        Destroy(other.gameObject);
+
+                        // Manage matching of blocks
+                        circleSegmentManager.ManageMatching(_slice, i+1);
+
+                        collision_handled = true;
+                    }
+                }
+
+            // If the player can reach the core of the planet
+            } else if (!collision_handled) {
+
+                // ----- TODO: Boss lose a life point ----- //
+                // ----- TODO: Update to phase 2 or 3 of Boss fight ----- //
+
+                collision_handled = true;
+
+            }
+
+            
+
+
+            /*
             GetComponent<SpriteRenderer>().color = other.GetComponent<SpriteRenderer>().color;
             Destroy(other.gameObject);
+            */
+            
+
+            
+
         }
     }
 }
