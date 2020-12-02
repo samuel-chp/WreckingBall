d7ed2c633af6873bf020e80cfac1a7046023135d
Breeky <samuelchaput87@gmail.com>
2020-12-02 19:12:10 +0100

    Fix controls and add options.

---
+++ Assets/FollowerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FollowerController : MonoBehaviour
+{
+    public Transform player;
+    public Transform planet;
+
+    // Must be under 180°
+    public float distanceAngle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 gravityDirection = planet.position - transform.position;
+        
+        // Rotation radial
+        if (transform.position.x > planet.position.x)
+            transform.rotation =  Quaternion.Euler(0, 0, -Vector2.Angle(Vector2.up, -gravityDirection));
+        else
+            transform.rotation = Quaternion.Euler(0, 0, Vector2.Angle(Vector2.up, -gravityDirection));
+
+        // Follow player
+        float currentAngle = Vector2.Angle(transform.position - planet.position, player.position - planet.position);
+        transform.RotateAround(planet.position, Vector3.forward, distanceAngle - currentAngle);
+    }
+}
---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using extOSC;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    
+    // OSC Connexion
+    private OSCTransmitter _transmitter;
+
+    public static AudioManager Instance;
+
+    private void Start()
+    {
+        // One AudioManager only
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        
+        // OSC initialization
+        _transmitter = GetComponent<OSCTransmitter>();
+    }
+
+    public void Play(string soundName)
+    {
+        // Find sound
+        Sound s = Array.Find(sounds, sound => sound.soundName == soundName);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound '"+soundName+"' not found!");
+            return;
+        }
+        
+        // Play sound
+        // Create message
+        var message = new OSCMessage("/playSound");
+        // Populate values.
+        message.AddValue(OSCValue.String("Default"));
+        message.AddValue(OSCValue.String(s.absolutePath));      // filename
+        message.AddValue(OSCValue.Float(s.volume)); // volume 
+        message.AddValue(OSCValue.Float(s.pitch));  // pitch
+        // Send message
+        _transmitter.Send(message);
+    }
+}
